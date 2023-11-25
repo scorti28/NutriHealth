ce87458fbb93fba4c62bbe548a4a8419b2728c03
andreeascortan28 <olivera.scortan@student.upt.ro>
2023-11-25 16:53:09 +0200

    TextBoxes added, but name from the Dashboard header is not diplayed. Work in progress

--- Authentification/src/Dashboard.js
+++ Authentification/src/Dashboard.js
@@ -1,10 +1,36 @@
-import { StyleSheet, SafeAreaView } from 'react-native';
+import { Text, TextInput, Button, StyleSheet, SafeAreaView } from 'react-native';
 import React, { useState, useEffect } from 'react';
-import Header from '../components/Header';
 import { firebase } from '../config';
 
 const Dashboard = () => {
   const [name, setName] = useState({ firstName: '', lastName: '' });
+  const [gender, setGender] = useState('');
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
+  const [age, setAge] = useState('');
+
+  const handleSaveData = () => {
+    const currentUser = firebase.auth().currentUser;
+
+    if (currentUser) {
+      // Create a reference to the 'users' collection in Firebase
+      const userRef = firebase.firestore().collection('users').doc(currentUser.uid);
+
+      // Save the entered data to the 'users' collection
+      userRef.set({
+        gender,
+        weight,
+        height,
+        age,
+      })
+        .then(() => {
+          console.log('Data saved successfully!');
+        })
+        .catch((error) => {
+          console.error('Error saving data:', error);
+        });
+    }
+  };
 
   useEffect(() => {
     const currentUser = firebase.auth().currentUser;
@@ -34,6 +60,43 @@ const Dashboard = () => {
 
   return (
     <SafeAreaView style={styles.container}>
+      <Text>Introduce your gender:</Text>
+      <TextInput
+        placeholder="Gender"
+        maxLength={6}
+        mode="flat"
+        style={styles.input}
+        value={gender}
+        onChangeText={(text) => setGender(text)}
+      />
+      <Text>Introduce your weight:</Text>
+      <TextInput
+        placeholder="Weight"
+        keyboardType="phone-pad"
+        mode="flat"
+        style={styles.input}
+        value={weight}
+        onChangeText={(text) => setWeight(text)}
+      />
+      <Text>Introduce your height:</Text>
+      <TextInput
+        placeholder="Height"
+        keyboardType="phone-pad"
+        mode="flat"
+        style={styles.input}
+        value={height}
+        onChangeText={(text) => setHeight(text)}
+      />
+      <Text>Introduce your age:</Text>
+      <TextInput
+        placeholder="Age"
+        keyboardType="phone-pad"
+        mode="flat"
+        style={styles.input}
+        value={age}
+        onChangeText={(text) => setAge(text)}
+      />
+      <Button title="Submit" onPress={handleSaveData} />
     </SafeAreaView>
   );
 };
@@ -45,15 +108,14 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     marginTop: 100,
-    backgroundColor: '#ffffff'
+    backgroundColor: '#ffffff',
   },
-  button: {
-    marginTop: 20,
-    height: 50,
-    width: 200,
-    backgroundColor: '#2ecc71',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 25
-  }
+  input: {
+    borderColor: 'gray',
+    width: '100%',
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
+  },
 });
